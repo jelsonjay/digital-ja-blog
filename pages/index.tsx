8204f2e5f0683b31301813fc4a4d92d134bb3dce
Jelson J <jelson20@outlook.com>
2021-08-10 23:09:56 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface IAppProps {}
+
+const App: React.FC<IAppProps> = (props) => {
+  return (
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+);
+};
+
+export default App;
